aef6a986986996d4b59f549859d41d40e5c7327a
guillaume <guillaume.lebot@free.fr>
2019-05-05 10:06:20 +0200

    projet api symfony 4

---
+++ src/Entity/Events.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\EventsRepository")
+ */
+class Events
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateEvent;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $participantsNumber;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDateEvent(): ?\DateTimeInterface
+    {
+        return $this->dateEvent;
+    }
+
+    public function setDateEvent(\DateTimeInterface $dateEvent): self
+    {
+        $this->dateEvent = $dateEvent;
+
+        return $this;
+    }
+
+    public function getParticipantsNumber(): ?int
+    {
+        return $this->participantsNumber;
+    }
+
+    public function setParticipantsNumber(int $participantsNumber): self
+    {
+        $this->participantsNumber = $participantsNumber;
+
+        return $this;
+    }
+}
